fd79883d12a54c3480e21df766f44bd2017b7a41
z9fr <z9fr@protonmail.com>
2023-01-05 01:15:58 +0530

    fix: cookie issues with login images

--- src/helpers/token.ts
+++ src/helpers/token.ts
@@ -2,28 +2,28 @@ import Cookies from "js-cookie";
 
 export const getAuthToken = (): string | null => Cookies.get("session") ?? null;
 export const getRefreshToken = (): string | null =>
-  Cookies.get("refreshToken") ?? null;
+  Cookies.get("admin_refreshToken") ?? null;
 
 export const setAuthToken = (token: string) =>
-  Cookies.set("session", token, {
+  Cookies.set("admin_session", token, {
     path: "/",
     domain: window.location.hostname,
     expires: 1 / 24, // 1 hour
   });
 
 export const setRefreshToken = (refreshToken: string) =>
-  Cookies.set("refreshToken", refreshToken, {
+  Cookies.set("admin_refreshToken", refreshToken, {
     path: "/",
     domain: window.location.hostname,
     expires: 1, // 1 day
   });
 
 export const clearAuthToken = () => {
-  Cookies.remove("session", {
+  Cookies.remove("admin_session", {
     path: "/",
     domain: window.location.hostname,
   });
-  Cookies.remove("refreshToken", {
+  Cookies.remove("admin_refreshToken", {
     path: "/",
     domain: window.location.hostname,
   });
